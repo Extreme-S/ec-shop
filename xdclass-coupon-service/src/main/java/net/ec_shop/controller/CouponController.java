1f44abada37a6c0ec56bfe6522a25ebb97d94cbe
不爱吃鱼的猫丶 <1716224950@qq.com>
2021-07-05 18:24:24 +0800

    优惠卷分页接口拦截器

--- xdclass-coupon-service/src/main/java/net/ec_shop/controller/CouponController.java
+++ xdclass-coupon-service/src/main/java/net/ec_shop/controller/CouponController.java
@@ -1,21 +1,47 @@
 package net.ec_shop.controller;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import net.ec_shop.service.CouponService;
+import net.ec_shop.util.JsonData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
- * @author 不爱吃鱼的猫丶
- * @since 2021-07-05
+ * @author 二当家小D
+ * @since 2021-02-07
  */
+@Api("优惠券模块")
 @RestController
-@RequestMapping("/couponDO")
+@RequestMapping("/api/coupon/v1")
 public class CouponController {
+
+
+    @Autowired
+    private CouponService couponService;
+
+
+    @ApiOperation("分页查询优惠券")
+    @GetMapping("page_coupon")
+    public JsonData pageCouponList(
+            @ApiParam(value = "当前页") @RequestParam(value = "page", defaultValue = "1") int page,
+            @ApiParam(value = "每页显示多少条") @RequestParam(value = "size", defaultValue = "10") int size
+    ) {
+
+        Map<String, Object> pageMap = couponService.pageCouponActivity(page, size);
+        return JsonData.buildSuccess(pageMap);
+    }
 
 }
 
---
+++ xdclass-coupon-service/src/main/java/net/ec_shop/service/CouponService.java
@@ -0,0 +1,15 @@
+package net.ec_shop.service;
+
+import java.util.Map;
+
+public interface CouponService {
+
+    /**
+     * 分页查询优惠券
+     *
+     * @param page
+     * @param size
+     * @return
+     */
+    Map<String, Object> pageCouponActivity(int page, int size);
+}
