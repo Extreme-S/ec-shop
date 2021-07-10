cab50500b93e5ce101690db9cd350662b6f53c64
不爱吃鱼的猫丶 <1716224950@qq.com>
2021-07-10 21:18:54 +0800

    初始化商品服务

---
+++ xdclass-product-service/src/main/java/net/ec_shop/mapper/ProductMapper.java
@@ -0,0 +1,16 @@
+package net.ec_shop.mapper;
+
+import net.ec_shop.model.ProductDO;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author 不爱吃鱼的猫丶
+ * @since 2021-07-10
+ */
+public interface ProductMapper extends BaseMapper<ProductDO> {
+
+}
---
+++ xdclass-product-service/src/main/java/net/ec_shop/model/BannerDO.java
@@ -0,0 +1,44 @@
+package net.ec_shop.model;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author 不爱吃鱼的猫丶
+ * @since 2021-07-10
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@TableName("banner")
+public class BannerDO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+      @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    /**
+     * 图片
+     */
+    private String img;
+
+    /**
+     * 跳转地址
+     */
+    private String url;
+
+    /**
+     * 权重
+     */
+    private Integer weight;
+
+
+}
